d3b3eb2698b24f964ab800c957c01d81bcde9e6b
aytajr <164667233+aytajr@users.noreply.github.com>
2025-03-15 22:28:55 +0400

    Add files via upload

---
+++ comparator-lambda/src/StudentComparator_task2.java
@@ -0,0 +1,24 @@
+import java.util.Comparator;
+
+public class StudentComparator_task2 implements Comparator<Student> {
+    @Override
+    public int compare(Student s1, Student s2) {
+        if (s1.getGrade() != s2.getGrade()) {
+            return Double.compare(s2.getGrade(), s1.getGrade());
+        }
+
+        if (s1.getBirthYear() != s2.getBirthYear()) {
+            return Integer.compare(s1.getBirthYear(), s2.getBirthYear());
+        }
+
+        if (!s1.getGender().equals(s2.getGender())) {
+            return s2.getGender().compareTo(s1.getGender());
+        }
+
+        if (!s1.getName().equals(s2.getName())) {
+            return s2.getName().compareTo(s1.getName());
+        }
+
+        return Integer.compare(s1.getId(), s2.getId());
+    }
+}
